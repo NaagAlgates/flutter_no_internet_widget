df731798dd012e6460c420e05f053da8ca5e07e1
Nagaraj Alagusundaram <me@nagaraj.com.au>
2022-08-02 18:13:22 +1000

    Added test cases for internet status

--- test/src/flutter_no_internet_widget_test.dart
+++ test/src/flutter_no_internet_widget_test.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:flutter_no_internet_widget/flutter_no_internet_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -11,6 +10,67 @@ void main() {
           online: Container(),
         ),
         isNotNull,
+      );
+    });
+
+    testWidgets('Pump FlutterNoInternetWidget', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: InternetWidget(
+            online: Container(),
+          ),
+        ),
+      );
+
+      expect(
+        find.byType(InternetWidget),
+        findsOneWidget,
+        reason: 'Load the widgets',
+      );
+    });
+
+    testWidgets('lookupUrl', (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: InternetWidget(
+            online: Container(),
+            lookupUrl: 'www.example.com',
+          ),
+        ),
+      );
+
+      expect(
+        find.byWidgetPredicate(
+          (widget) =>
+              widget is InternetWidget &&
+              widget.lookupUrl != null &&
+              widget.lookupUrl!.contains('www.example.com'),
+        ),
+        findsOneWidget,
+      );
+    });
+
+    testWidgets('loadingWidget', (WidgetTester tester) async {
+      const _loadingWidget = Center(
+        child: CircularProgressIndicator(),
+      );
+      await tester.pumpWidget(
+        MaterialApp(
+          home: InternetWidget(
+            online: Container(),
+            loadingWidget: _loadingWidget,
+          ),
+        ),
+      );
+
+      expect(
+        find.byWidgetPredicate(
+          (widget) =>
+              widget is InternetWidget &&
+              widget.loadingWidget != null &&
+              widget.loadingWidget == _loadingWidget,
+        ),
+        findsOneWidget,
       );
     });
   });
